ca80be5b396aeb619395b0fa547abbc62362929d
Arecio Canton <cuba289@gmail.com>
2021-04-16 16:23:33 -0400

    exported modules, fixed Intern fileName, created new classes for each team member

---
+++ index.js
@@ -0,0 +1,6 @@
+const Manger = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employees");
+const Engineer = require("./lib/Engineer");
+const Inquirer = require("Inquirer");
+const fs = require("fs");
