9e850370066b2e335b443b39580108cc475e9b5e
Arecio Canton <cuba289@gmail.com>
2021-04-18 17:40:51 -0400

    added manager test

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -3,9 +3,19 @@ const Manager = require("../lib/Manager");
 
 describe("Manager", () => {
     describe("Extend", () => {
-        it("Should add a new property for office number", () => {
-            const manager = new Manager("404A");
-            expect(manager).toEqual("404A");
-        })
+        it("Should add a employee called manager with the office number property", () => {
+            const manager = new Manager("Arecio", 123, "arecio@gmail.com","404A");
+            expect(manager).toEqual({name: "Arecio", id: 123, email: "arecio@gmail.com", officeNumber: "404A"});
+        });
+    });
+
+    test ("Can get role with getRole function", () => {
+        const manager = new Manager("Manager");
+        expect(manager.getRole()).toBe("Manager");
+    })
+
+    test ("Can get office number with officeNumber function", () => {
+        const manager = new Manager("404A");
+        expect(manager.getOfficeNumber()).toBe(manager.officeNumber);
     })
 })
